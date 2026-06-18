cb305d1a587c533db67814a167aed35664471438
benedettaStef <84447936+benedettaStef@users.noreply.github.com>
2026-06-18 09:21:18 +0200

    Delete marine_emergency_fhir_system directory

--- marine_emergency_fhir_system/exception/PatientNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.benedetta.marine_emergency_fhir_system.exception;
-
-
-public class PatientNotFoundException extends RuntimeException {
-
-    public PatientNotFoundException(Long id) {
-        super("Patient with id " + id + " not found");
-    }
-}
--- marine_emergency_fhir_system/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.benedetta.marine_emergency_fhir_system.repository;
-
-import com.benedetta.marine_emergency_fhir_system.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
